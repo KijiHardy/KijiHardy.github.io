b9379aedd321e146b0ee7b6d6d2fd13e8e19355d
Da'shawn Hardy <dashawnhardy09@gmail.com>
2025-11-06 13:15:12 -0500

    Add files via upload

---
+++ projects/js/js03/chapter/js03.js
@@ -0,0 +1,83 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Da'shawn Hardy
+     Date:   11/3/2025
+
+     Filename: js03.js
+ */
+
+     let weeekDays =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+     window.addEventListener("load", addWeekDays());
+     // function to write weekday names into the calendar 
+     function addWeekDays(){
+        let i = 0; //initial counter value
+     
+
+     // reference the collection of heading cells
+     let headingCells = document.getElementsByTagName("th");
+
+     //write each of the seven days into a heading cell
+     while(i < 7){
+        headingCells[i].innerHTML = weeekDays[i];
+
+        //increase the counter by 1
+        i++;
+     }
+}
+
+window.addEventListener("load", showGames);
+//function to write game information into the calendar
+function showGames(){
+    for(let i = 0; i < gameDates.length; i++){
+        let gameInfo = "";
+
+       switch (gameResults[i]) {
+        case "W":
+            gameInfo += "<p class='win'>";
+            break;
+        case "L":
+            gameInfo += "<p class='lose'>";
+            break;
+        case "S":  
+            gameInfo += "<p class='suspended'>";
+            break;
+        case "P":
+            gameInfo += "<p class='postponed'>";
+            break;
+       }
+
+        // Display the game location
+        if(gameLocations[i] === "h"){
+            gameInfo += "vs. ";
+        } else if(gameLocations[i] === "a"){
+            gameInfo += "@ ";
+        }
+        //include the opponent
+        gameInfo += gameOpponents[i] + "<br>";
+
+        //include the result and score
+        gameInfo += gameResults[i] + ": (" + runsScored[i] + " - " + runsAllowed[i] + ")";
+
+        // Display innings played for suspended, shortened, or extrainning games
+        if(gameInnings[i] < 5){
+            gameInfo += " [" + gameInnings[i]+"]***";
+        } else if(gameInnings[i] < 9){
+            gameInfo +=" [" + gameInnings[i]+"]*";
+        } else if(gameInnings[i] > 9){
+            gameInfo +=" [" + gameInnings[i]+"]";
+        }
+        //close the paragraph
+
+        gameInfo +="</p>";
+
+        //write the information into a table cell
+
+        let tableCell = document.getElementById(gameDates[i]);
+        tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+    }
+}
+
